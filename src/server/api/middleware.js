01de2171aa495157ee265c95e2a9bbe7ddc8caf1
Bima Saridjo <palessie27@gmail.com>
2020-09-03 12:48:11 -0400

    added express middleware.js

---
+++ src/server/api/middleware.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+import app from './server';
+
+const DIST_PATH = path.join(__dirname, '../../../dist');
+const PUBLIC_PATH = path.join(__dirname, '../../../public');
+
+app.use(express.json());
+
+const expressMiddleware = () => {
+  app.use(express.static(DIST_PATH));
+  app.use(express.static(PUBLIC_PATH));
+};
+
+module.exports = expressMiddleware;
