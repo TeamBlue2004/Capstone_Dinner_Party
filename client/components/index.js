5e0e9c8a5f8e1068f24b24d2dc8ac7bbee13c18a
JudithBLinder <judithrooz@gmail.com>
2020-09-08 19:19:11 -0400

    fixInitBoiler

---
+++ client/components/index.js
@@ -0,0 +1,6 @@
+// Import and Export all components from one file
+import Header from './header/Header';
+import Home from './home/Home';
+import Events from './events/Events';
+
+export { Header, Home, Events };
