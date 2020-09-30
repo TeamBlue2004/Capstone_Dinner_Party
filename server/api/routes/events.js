917a201f53147691bd9671cfec7243ca2c06a823
JudithBLinder <judithrooz@gmail.com>
2020-09-30 11:03:28 -0400

    express validator on event routes

--- server/api/routes/events.js
+++ server/api/routes/events.js
@@ -78,6 +78,7 @@ eventsRouter.get('/events/eventGuests/:eventId', async (req, res) => {
 eventsRouter.post(
   '/events',
   [
+    check('hostId', 'hostId is required').not().isEmpty(),
     check('eventName', 'Event name is required').not().isEmpty(),
     check('datetime', 'Date and time is required').isISO8601().toDate(),
     check('location', 'Location is required').not().isEmpty(),
@@ -117,52 +118,82 @@ eventsRouter.post(
       res.status(200).send(event);
     } catch (e) {
       console.error(chalk.red(e));
-      res.status(500).send({ message: 'Server Error' });
+      res.status(500).send({ message: 'Server Error while adding event' });
     }
   }
 );
 
-eventsRouter.put('/events/accept', async (req, res) => {
-  const { userId, eventId } = req.body;
-  try {
-    const event = await Event.findOne({
-      where: {
-        id: eventId,
-      },
-    });
-    const user = await User.findOne({
-      where: {
-        id: userId,
-      },
-    });
-    event.addUser(user, { through: { status: 'Approved' } });
-    res.sendStatus(200);
-  } catch (e) {
-    console.error(e);
-    res.status(500).send({ message: 'Server error while deleting event' });
-  }
-});
+eventsRouter.put(
+  '/events/accept',
+  [
+    check('userId', 'userId is required').not().isEmpty(),
+    check('eventId', 'eventId is required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-eventsRouter.put('/events/decline', async (req, res) => {
-  const { userId, eventId } = req.body;
-  try {
-    const event = await Event.findOne({
-      where: {
-        id: eventId,
-      },
-    });
-    const user = await User.findOne({
-      where: {
-        id: userId,
-      },
-    });
-    event.addUser(user, { through: { status: 'Declined' } });
-    res.sendStatus(200);
-  } catch (e) {
-    console.error(e);
-    res.status(500).send({ message: 'Server error while deleting event' });
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const { userId, eventId } = req.body;
+    try {
+      const event = await Event.findOne({
+        where: {
+          id: eventId,
+        },
+      });
+      const user = await User.findOne({
+        where: {
+          id: userId,
+        },
+      });
+      event.addUser(user, { through: { status: 'Approved' } });
+      res.sendStatus(200);
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({ message: 'Server error while updating event' });
+    }
   }
-});
+);
+
+eventsRouter.put(
+  '/events/decline',
+  [
+    check('userId', 'userId is required').not().isEmpty(),
+    check('eventId', 'eventId is required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const { userId, eventId } = req.body;
+    try {
+      const event = await Event.findOne({
+        where: {
+          id: eventId,
+        },
+      });
+      const user = await User.findOne({
+        where: {
+          id: userId,
+        },
+      });
+      event.addUser(user, { through: { status: 'Declined' } });
+      res.sendStatus(200);
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({ message: 'Server error while updating event' });
+    }
+  }
+);
 
 eventsRouter.delete('/events/:id', async (req, res) => {
   const { id } = req.params;
